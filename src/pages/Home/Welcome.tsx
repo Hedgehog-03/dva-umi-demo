cf6714ecec371f92de1b292b2150c42bedf1ac71
Hedgehog-03 <1351505656@qq.com>
2022-04-29 18:08:27 +0800

    feat: 尝试开发图片全屏展示组件

--- src/pages/Home/Welcome.tsx
+++ src/pages/Home/Welcome.tsx
@@ -1,7 +1,25 @@
+import { useState } from 'react';
+import { Modal } from 'antd';
 function Welcome() {
+  const [visible, setVisible] = useState(false);
   return (
     <div>
-      Welcome
+      <img
+        id="img"
+        onClick={() => setVisible(!visible)}
+        style={{ cursor: 'zoom-in' }}
+        src="https://zos.alipayobjects.com/rmsportal/PPrerEAKbIoDZYr.png"
+        alt=""
+      />
+      <Modal visible={visible} centered footer={null}>
+      <img
+        id="img"
+        // onClick={() => setVisible(!visible)}
+        // style={{ cursor: 'zoom-in' }}
+        src="https://zos.alipayobjects.com/rmsportal/PPrerEAKbIoDZYr.png"
+        alt=""
+      />
+      </Modal>
     </div>
   );
 }
